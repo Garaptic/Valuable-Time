0d96086fef5addcd8025b445ac9b8dcc4949d258
Garaptic <garaptic@gmail.com>
2026-07-23 22:58:12 +0300

    Ещё какая-то хрень

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,37 @@ public class PlayerController : MonoBehaviour
     public float upForce = 200f;
 
     public Rigidbody2D rb;
+    public Animator animator; // 1. Ссылка на компонент Animator
+
     float horizontalMovement;
     bool isSprinting;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        // 2. Автоматически находим Animator, если забыли перетащить в инспекторе
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         float currentSpeed = isSprinting ? moveSpeed * sprintMultiplier : moveSpeed;
         rb.linearVelocity = new Vector2(horizontalMovement * currentSpeed, rb.linearVelocity.y);
+
+        // 3. Передаем абсолютную скорость движения в аниматор (без учета знака "-" при движении влево)
+        float horizontalSpeed = Mathf.Abs(rb.linearVelocity.x);
+        animator.SetFloat("Speed", horizontalSpeed);
+
+        // 4. Поворот спрайта в сторону движения (Flip)
+        if (horizontalMovement > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (horizontalMovement < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 
     void OnMove(InputValue value)
